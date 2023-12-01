c29a5bf8493fccefc35b971cd6e0338e5ba4dba7
Ianis <bostiogianis@tutanota.com>
2023-12-01 15:04:39 +0200

    C calculator

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+float numarul1;
+float numarul2;
+char operator;
+float rezultat;
+
+int debug()
+{
+    printf("DEBUG: \n");
+    printf("Primul numar %f \nAl doilea numar %f Operatorul de calcul: %c",numarul1,numarul2,operator);
+}
+
+int main()
+{
+    printf("Calculator scris in C 1.12.2023 \n");
+    printf("Introduceti primul numar: \n");
+    scanf("%f",&numarul1);
+    printf("Introduceti al doilea numar: \n");
+    scanf("%f",&numarul2);
+    printf("Introduceti operatorul de calcul (+,-,/,*): \n");
+    scanf("%s",&operator);
+
+    switch (operator)
+    {
+        case '+':
+            rezultat = numarul1 + numarul2;
+            printf("%g + %g = %g\n",numarul1,numarul2,rezultat);
+            break;
+        case '-':
+            rezultat = numarul1 - numarul2;
+            printf("%g - %g = %g\n",numarul1,numarul2,rezultat);
+            break;
+        case '/':
+            rezultat = numarul1 / numarul2;
+            printf("%g / %g = %g\n",numarul1,numarul2,rezultat);
+            break;
+        case '*':
+            rezultat = numarul1 * numarul2;
+            printf("%g * %g = %g\n",numarul1,numarul2,rezultat);
+            break;
+        default:
+            printf("Acesta nu este un operator matematic \n");
+            break;
+    }
+    // debug();
+}
+
